114256cb96b7bac25cd112eb880f489c7b6ed31f
Nikolas Wolfe <nikwolfe7@gmail.com>
2016-05-14 15:41:40 -0400

    stuff

--- src/example/TrainNeuralNetworkDriver.java
+++ src/example/TrainNeuralNetworkDriver.java
@@ -14,40 +14,50 @@ import mlsp.cs.cmu.edu.dnn.util.MaxBinaryThresholdOutput;
 import mlsp.cs.cmu.edu.dnn.util.OutputAdapter;
 
 public class TrainNeuralNetworkDriver {
-	
+
 	static int numCores = Runtime.getRuntime().availableProcessors();
 	static ExecutorService pool = Executors.newFixedThreadPool(numCores);
 	static String o = "mnist-test";
 	static boolean batch = false;
 	static boolean allow = true;
 	static boolean saveSnapshots = true;
-	static int snapshotInterval = 5; 
+	static int snapshotInterval = 5;
 	static int iterations = 1000;
 	static double minDiff = 1.0e-8;
-	
-	private static void runDNN(String o, int maxIter, boolean batch, boolean ss, int si, int... structure) throws IOException {
+
+	private static void runDNN(String o, int maxIter, boolean batch, boolean ss, int si, int... structure)
+			throws IOException {
 		OutputAdapter adapter = new MaxBinaryThresholdOutput();
 		DataInstanceFactory dataInstanceFactory = new MNISTDataInstanceFactory();
 		List<DataInstance> training = dataInstanceFactory.getTrainingInstances();
- 		List<DataInstance> testing = dataInstanceFactory.getTestingInstances();
- 		DNNFactory dnnFactory = new CustomDNNFactory(testing.get(0), structure);
-		Thread dnnThread = new TrainNetworkThread(o, adapter, dataInstanceFactory, training, testing, dnnFactory, minDiff, -1, allow, 1, maxIter, batch, ss, si, structure);
+		List<DataInstance> testing = dataInstanceFactory.getTestingInstances();
+		DNNFactory dnnFactory = new CustomDNNFactory(testing.get(0), structure);
+		Thread dnnThread = new TrainNetworkThread(o, adapter, dataInstanceFactory, training, testing, dnnFactory,
+				minDiff, -1, allow, 1, maxIter, batch, ss, si, structure);
 		pool.execute(dnnThread);
 	}
-	
+
 	public static void main(String[] args) throws IOException {
-		boolean ss; int si; 
-		ss = saveSnapshots;
-		si = snapshotInterval; 
-		runDNN(o, iterations, batch, ss, si, new int[] {16});
-		runDNN(o, iterations, batch, ss, si, new int[] {100});
-		runDNN(o, iterations, batch, ss, si, new int[] {28,72});
-		runDNN(o, iterations, batch, ss, si, new int[] {72,28});
-		runDNN(o, iterations, batch, ss, si, new int[] {50,50});
-		runDNN(o, iterations, batch, ss, si, new int[] {56,28,16});
-		runDNN(o, iterations, batch, ss, si, new int[] {16,56,28});
-		runDNN(o, iterations, batch, ss, si, new int[] {28,56,16});
+		runDNN(o, iterations, batch, saveSnapshots, snapshotInterval, new int[] { 16 });
+		runDNN(o, iterations, batch, saveSnapshots, snapshotInterval, new int[] { 100 });
+		runDNN(o, iterations, batch, saveSnapshots, snapshotInterval, new int[] { 28, 72 });
+		runDNN(o, iterations, batch, saveSnapshots, snapshotInterval, new int[] { 72, 28 });
+		runDNN(o, iterations, batch, saveSnapshots, snapshotInterval, new int[] { 50, 50 });
+		runDNN(o, iterations, batch, saveSnapshots, snapshotInterval, new int[] { 56, 28, 16 });
+		runDNN(o, iterations, batch, saveSnapshots, snapshotInterval, new int[] { 16, 56, 28 });
+		runDNN(o, iterations, batch, saveSnapshots, snapshotInterval, new int[] { 28, 56, 16 });
 		pool.shutdown();
 	}
 	
+	/*
+	 * runDNN(o, iterations, batch, new int[] {100}); 			// 79400 -- 1.)	991s **
+		runDNN(o, iterations, batch, new int[] {72,28}); 		// 58744 -- 2.)	754s
+		runDNN(o, iterations, batch, new int[] {56,28,16});		// 46080 -- 3.)	593s
+		runDNN(o, iterations, batch, new int[] {50,50});		// 42200 -- 4.)	522s
+		runDNN(o, iterations, batch, new int[] {28,72}); 		// 24688 -- 5.)	207s **
+		runDNN(o, iterations, batch, new int[] {28,56,16});		// 24576 -- 6.)	99s
+		runDNN(o, iterations, batch, new int[] {16,56,28});		// 15288 -- 7.)	76s
+		runDNN(o, iterations, batch, new int[] {16}); 			// 12704 -- 8.) 44s
+	 * */
+
 }
