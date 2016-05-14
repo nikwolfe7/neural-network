3843693606e46c6ffc1558a079b012c4008ede1d
Nikolas Wolfe <nikwolfe7@gmail.com>
2016-05-14 15:28:50 -0400

    some stuff...

---
+++ src/test2/TrainNetworkThread.java
@@ -0,0 +1,97 @@
+package test2;
+
+import java.io.IOException;
+import java.util.List;
+
+import mlsp.cs.cmu.edu.dnn.factory.CrossEntropyNetworkElementFactory;
+import mlsp.cs.cmu.edu.dnn.factory.DNNFactory;
+import mlsp.cs.cmu.edu.dnn.factory.ReadSerializedFileDNNFactory;
+import mlsp.cs.cmu.edu.dnn.factory.SigmoidNetworkElementFactory;
+import mlsp.cs.cmu.edu.dnn.factory.SigmoidNetworkFFDNNFactory;
+import mlsp.cs.cmu.edu.dnn.structure.NeuralNetwork;
+import mlsp.cs.cmu.edu.dnn.training.DNNTrainingModule;
+import mlsp.cs.cmu.edu.dnn.training.DataInstance;
+import mlsp.cs.cmu.edu.dnn.training.DataInstanceFactory;
+import mlsp.cs.cmu.edu.dnn.util.OutputAdapter;
+
+public class TrainNetworkThread extends Thread {
+
+	static int idNum = 0;
+	static boolean printOut = true;
+	private DNNTrainingModule trainingModule;
+	private DNNFactory dnnFactory;
+	private NeuralNetwork net;
+	private List<DataInstance> testingSet;
+	private List<DataInstance> trainingSet;
+	private OutputAdapter adapter;
+	private String netFile;
+
+	public TrainNetworkThread
+			(/* output */
+			String netFile, 
+			OutputAdapter adapter, 
+			DataInstanceFactory dataFactory, 
+			List<DataInstance> training,
+			List<DataInstance> testing, 
+			DNNFactory dnnFactory,
+			/* convergence criteria */
+			double minDiff, 
+			double minSquaredError, 
+			boolean allowNegativeIterations, 
+			int numMinChangeIterations,
+			int maxIterations,
+			boolean batchUpdate,
+			int... structure) throws IOException {
+
+		this.adapter = adapter;
+		this.trainingSet = training;
+		this.testingSet = testing;
+		DataInstance ex = testing.get(0);
+		this.net = dnnFactory.getInitializedNeuralNetwork();
+		this.trainingModule = new DNNTrainingModule(net, trainingSet, testingSet);
+		trainingModule.setOutputAdapter(adapter);
+		trainingModule.setOutputOn(printOut);
+		trainingModule.setBatchUpdate(batchUpdate);
+		trainingModule.setConvergenceCriteria(minDiff, minSquaredError, allowNegativeIterations,
+				numMinChangeIterations, maxIterations);
+
+		String[] arr = new String[] { netFile, "in-" + ex.getInputDimension(), "out-" + ex.getOutputDimension(),
+				"struct-" + str(structure) + "-id-" + (++idNum)};
+		this.netFile = String.join("-", arr) + ".dnn";
+		System.out.println("Training network for file: " + this.netFile);
+	}
+
+	private String str(int... vals) {
+		String[] arr = new String[vals.length];
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = "" + vals[i];
+		}
+		return String.join("-", arr);
+	}
+
+	private void trainNetwork() {
+		trainingModule.doTrainNetworkUntilConvergence();
+	}
+
+	private void testAndSaveNetwork() {
+		trainingModule.doTestTrainedNetwork();
+		trainingModule.saveNetworkToFile(netFile);
+	}
+
+	private void verifySavedNetwork() {
+		System.out.println("De-serializing network...");
+		dnnFactory = new ReadSerializedFileDNNFactory(netFile);
+		net = dnnFactory.getInitializedNeuralNetwork();
+		trainingModule = new DNNTrainingModule(net, testingSet);
+		trainingModule.setOutputAdapter(adapter);
+		trainingModule.setOutputOn(printOut);
+		trainingModule.doTestTrainedNetwork();
+	}
+
+	@Override
+	public void run() {
+		trainNetwork();
+		testAndSaveNetwork();
+		verifySavedNetwork();
+	}
+}
---
+++ src/test2/TrainNeuralNetworkDriver.java
@@ -0,0 +1,48 @@
+package test2;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+import mlsp.cs.cmu.edu.dnn.factory.DNNFactory;
+import mlsp.cs.cmu.edu.dnn.training.DataInstance;
+import mlsp.cs.cmu.edu.dnn.training.DataInstanceFactory;
+import mlsp.cs.cmu.edu.dnn.training.MNISTDataInstanceFactory;
+import mlsp.cs.cmu.edu.dnn.util.MaxBinaryThresholdOutput;
+import mlsp.cs.cmu.edu.dnn.util.OutputAdapter;
+
+public class TrainNeuralNetworkDriver {
+	
+	static int numCores = Runtime.getRuntime().availableProcessors();
+	static ExecutorService pool = Executors.newFixedThreadPool(numCores);
+	static String o = "mnist-test";
+	static boolean batch = false;
+	static boolean allow = true;
+	static int iterations = 1000;
+	static double minDiff = 1.0e-8;
+	
+	private static void runDNN(String o, int maxIter, boolean batch, int... structure) throws IOException {
+		OutputAdapter adapter = new MaxBinaryThresholdOutput();
+		DataInstanceFactory dataInstanceFactory = new MNISTDataInstanceFactory();
+		List<DataInstance> training = dataInstanceFactory.getTrainingInstances();
+ 		List<DataInstance> testing = dataInstanceFactory.getTestingInstances();
+ 		DNNFactory dnnFactory = new CustomDNNFactory(testing.get(0), structure);
+		Thread dnnThread = new TrainNetworkThread(o, adapter, dataInstanceFactory, training, testing, dnnFactory, minDiff, -1, allow, 1, maxIter, batch, structure);
+		pool.execute(dnnThread);
+	}
+	
+	public static void main(String[] args) throws IOException {
+		runDNN(o, iterations, batch, new int[] {100}); 			// 79400 -- 1.)	991s
+		runDNN(o, iterations, batch, new int[] {72,28}); 		// 58744 -- 2.)	754s
+		runDNN(o, iterations, batch, new int[] {56,28,16});		// 46080 -- 3.)	593s
+		runDNN(o, iterations, batch, new int[] {50,50});		// 42200 -- 4.)	522s
+		runDNN(o, iterations, batch, new int[] {28,72}); 		// 24688 -- 5.)	207s
+		runDNN(o, iterations, batch, new int[] {28,56,16});		// 24576 -- 6.)	99s
+		runDNN(o, iterations, batch, new int[] {16,56,28});		// 15288 -- 7.)	76s
+		runDNN(o, iterations, batch, new int[] {16}); 			// 12704 -- 8.) 44s
+		
+		pool.shutdown();
+	}
+	
+}
