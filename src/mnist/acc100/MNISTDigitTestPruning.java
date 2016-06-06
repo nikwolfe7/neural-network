ef92f6c2c47d7179f6eb4caf262264442cac2c15
Nikolas Wolfe <nikwolfe7@gmail.com>
2016-06-06 09:30:12 -0400

    asdfasdfasdf

---
+++ src/mnist/acc100/MNISTDigitTestPruning.java
@@ -0,0 +1,54 @@
+package mnist.acc100;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+import mlsp.cs.cmu.edu.dnn.factory.DNNFactory;
+import mlsp.cs.cmu.edu.dnn.factory.ReadSerializedFileDNNFactory;
+import mlsp.cs.cmu.edu.dnn.structure.NeuralNetwork;
+import mlsp.cs.cmu.edu.dnn.training.CosineGenerator;
+import mlsp.cs.cmu.edu.dnn.training.DNNTrainingModule;
+import mlsp.cs.cmu.edu.dnn.training.DataInstance;
+import mlsp.cs.cmu.edu.dnn.training.DataInstanceFactory;
+import mlsp.cs.cmu.edu.dnn.training.DataInstanceGenerator;
+import mlsp.cs.cmu.edu.dnn.training.DataReader;
+import mlsp.cs.cmu.edu.dnn.training.MNISTAltSmallDataInstanceFactory;
+import mlsp.cs.cmu.edu.dnn.training.MNISTSmallDataInstanceFactory;
+import mlsp.cs.cmu.edu.dnn.training.ReadCSVTrainingData;
+import mlsp.cs.cmu.edu.dnn.training.XORGenerator;
+import mlsp.cs.cmu.edu.dnn.util.BinaryThresholdOutput;
+import mlsp.cs.cmu.edu.dnn.util.DNNUtils;
+import mlsp.cs.cmu.edu.dnn.util.MaxBinaryThresholdOutput;
+import mlsp.cs.cmu.edu.dnn.util.OutputAdapter;
+import mlsp.cs.cmu.edu.dnn.util.PruningTool;
+
+public class MNISTDigitTestPruning {
+
+	static String dnnFile = "mnist-acc99-in-400-out-10-struct-100-id-1.dnn";
+
+	public static void main(String[] args) throws IOException {
+		TestPruning();
+	}
+
+	public static void TestPruning() throws IOException {
+		System.out.println("---------------------------------------------------");
+		System.out.println("               MNIST Digit Pruning                 ");
+		System.out.println("---------------------------------------------------");
+		
+		OutputAdapter adapter = new MaxBinaryThresholdOutput();
+		DataInstanceFactory dataInstanceFactory = new MNISTAltSmallDataInstanceFactory();
+		List<DataInstance> training = dataInstanceFactory.getTrainingInstances();
+		List<DataInstance> testing = dataInstanceFactory.getTestingInstances();
+
+		System.out.println("Deserializing stored network " + dnnFile);
+		DNNFactory factory = new ReadSerializedFileDNNFactory("models" + DNNUtils.sep + dnnFile);
+		NeuralNetwork net = factory.getInitializedNeuralNetwork();
+		DNNTrainingModule trainingModule = new DNNTrainingModule(net, testing);
+		trainingModule.setOutputOn(false);
+		trainingModule.setOutputAdapter(adapter);
+		trainingModule.doTestTrainedNetwork();
+		net = PruningTool.runPruningExperiment(dnnFile, true, net, training, testing, 1.0);
+	}
+
+}
